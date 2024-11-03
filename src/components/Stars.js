2965e4fd3c543b7441bad479634eeb9b856eb9e6
Ashley Corona <ashleeglasss@gmail.com>
2024-11-03 15:47:27 -0500

    layout/star styles

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -4,10 +4,26 @@ import { db } from "@/app/firebaseConfig";
 
 const Stars = ({ postId }) => {
   const [starred, setStarred] = useState(false);
+  const [starCount, setStarCount] = useState(0);
 
   useEffect(() => {
+    const fetchStarCount = async () => {
+      try {
+        const postRef = doc(db, "blogPosts", postId);
+        const postSnap = await getDoc(postRef);
+        if (postSnap.exists()) {
+          const data = postSnap.data();
+          setStarCount(data.stars || 0);
+        }
+      } catch (error) {
+        console.error("Error fetching star count:", error);
+      }
+    };
+
     const isStarred = localStorage.getItem(`starred_${postId}`);
     setStarred(isStarred === "true");
+
+    fetchStarCount();
   }, [postId]);
 
   const handleStarClick = async () => {
@@ -19,6 +35,7 @@ const Stars = ({ postId }) => {
         stars: increment(1),
       });
       setStarred(true);
+      setStarCount((prevCount) => prevCount + 1);
       localStorage.setItem(`starred_${postId}`, "true");
     } catch (error) {
       console.error("Error updating stars:", error);
@@ -26,22 +43,32 @@ const Stars = ({ postId }) => {
   };
 
   return (
-    <button 
-    onClick={handleStarClick} 
-    disabled={starred}
-    style={{
-      color: '#fff',
-      backgroundColor: '#000',
-      height: 25,
-      width: 35,
-      margin: 2,
-      marginTop: 15,
-      marginbotton: 15,
-      cursor: starred ? "not-allowed" : "pointer"
-    }}
-    >
-      {starred ? "ty ily" : "✰"}
-    </button>
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <div>
+        <button
+          onClick={handleStarClick}
+          disabled={starred}
+          style={{
+            color: "#ff00c3",
+            backgroundColor: "#000",
+            height: "7vh",
+            width: "12vh",
+            marginLeft: "10vh",
+            marginBottom: "5vh",
+            marginTop: "5vh",
+            margin: "2vh",
+            cursor: starred ? "not-allowed" : "pointer",
+          }}
+        >
+          {starred ? "ty ily" : "✰"}
+        </button>
+        {starred && (
+          <span>
+            ✰ {starCount}
+          </span>
+        )}
+      </div>
+    </div>
   );
 };
 
