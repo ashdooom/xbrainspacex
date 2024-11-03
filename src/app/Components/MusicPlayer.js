945c1a82cfd3064c83915ee93e2b4cdce8fc7a7b
Ashley Corona <ashleeglasss@gmail.com>
2024-11-02 21:52:41 -0400

    first commit

---
+++ src/app/Components/MusicPlayer.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { Howl } from 'howler';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
+
+function MusicPlayer() {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [currentTrack] = useState({ title: 'THE FIRST LADY OF JUICY COUTURE!!!!', url: '/music/juicyCouture.mp3' });
+
+    const sound = new Howl({
+        src: [currentTrack.url],
+        autoplay: false,
+    });
+
+    useEffect(() => {
+        if (isPlaying) {
+            sound.play();
+        } else {
+            sound.pause();
+        }
+
+        return () => sound.unload();
+    }, [isPlaying]);
+
+    const togglePlayPause = () => {
+        setIsPlaying(!isPlaying);
+    };
+
+    return (
+        <div className="musicPlayer" style={{ display: 'flex', alignItems: 'center', gap: '1vh' }}>
+            <div>{currentTrack.title}</div>
+            <FontAwesomeIcon
+                icon={isPlaying ? faPause : faPlay}
+                onClick={togglePlayPause}
+                style={{ cursor: 'pointer', fontSize: '1rem', marginLeft: '1vh', marginRight: '7vh' }}
+            />
+        </div>
+    );
+}
+
+export default MusicPlayer;
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,115 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import xbrainstewx from '/public/images/xbrainstewx.png';
+import selfie from '/public/images/selfie.webp';
+import onlineNow from '/public/images/onlineNow.gif';
+import marioStar from '/public/images/marioStar.gif';
+import diary from '/public/images/diary.png';
+import mew from '/public/images/mew.gif';
+import MusicPlayer from "./Components/MusicPlayer";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div className={styles.pageContainer}>
+          <div>
+            <Image className={styles.brain} src={xbrainstewx} />
+          </div>
+          <div>
+            <p className={styles.promo}>
+              want a website like this? need some artwork or a logo? visit my page at <a target="_blank" rel="noopener noreferrer" className={styles.brainLink} href="https://xbrainstewx.com">xbrainstewx.com</a>
+            </p>
+          </div>
+          <div className={styles.boxContainer}>
+            <div className={styles.aboutMe}>
+              <div className={styles.aboutContainer}>
+                <div className={styles.selfieMusic}>
+                  <Image className={styles.selfie} src={selfie} />
+                  <div className={styles.music}>
+                    <MusicPlayer />
+                  </div>
+                </div>
+                <div className={styles.aslContainer}>
+                  <Image className={styles.marioStar} src={marioStar} />
+                  <h1 className={styles.name}>
+                    ashdooom!
+                  </h1>
+                  <h3 className={styles.asl}>
+                    age: 27
+                    <br />
+                    sex: female
+                    <br />
+                    location: CT
+                  </h3>
+                  <div>
+                    <Image className={styles.onlineNow} src={onlineNow} />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.diary}>
+              <div className={styles.diaryContainer}>
+                <Image className={styles.diaryLogo} src={diary} />
+                <div className={styles.diaryBoxContainer}>
+                  <div className={styles.diaryBox}>
+                    <div className={styles.diaryText}>
+                      <div className={styles.dateMood}>
+                        <Image className={styles.mew} src={mew} />
+                        <br />
+                        2.24.23
+                        <br />
+                        <br />
+                        mood: excited!
+                      </div>
+                      <br />
+                      <br />
+                      new diary entry! i havent updated this since i launched my little website. i had a few bugs here
+                      and there
+                      so my page was down for maintenance. i actually had a blast creating the construction page i
+                      cooked up. ive
+                      definitely improved and sped up. my awesome and sexy husband Ryan is helping me sign up for a
+                      coding
+                      bootcamp to help turn me from a freelance fun coder to a full stack web developer in only 11
+                      months. im so
+                      nervous and i have a nasty case of imposter syndrome. then again how many people can say they
+                      built their
+                      own website from scratch top to bottom using 100% of their own code? not to disparage people who
+                      use
+                      websites to build their websites. but there is something magical and satisfying to create
+                      something with
+                      your own two hands. anyway im talking too much .. thanks for checking out my website. next step
+                      is to
+                      intergrate some mf JAVASCRIPT in this bitch. nervous but excited :3 smell ya later
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <div className={styles.dateMood}>
+                        1.11.23
+                        <br />
+                        <br />
+                        mood: accomplished + exhausted
+                      </div>
+                      <br />
+                      <br />
+                      yo im almost done building my website. id like to shoutout redbull, youtube, and my little hands
+                      for helpin
+                      me out with this fun project. my husband ryan also gets lots of props for supporting, cherishing
+                      and pushing
+                      me through my school werk and the building of this website. this page is soley for the girls
+                      gays and theys.
+                      enjoy my little babies.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
